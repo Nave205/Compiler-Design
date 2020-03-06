63361bdb66e640551e6b87098ec483e0d314c492
Nave205 <61616317+Nave205@users.noreply.github.com>
2020-03-06 13:54:23 +0800

    Improved (after Phase 2 checking)
    -new reserved words
    -corrected token names

--- Cocoa Coding/src/ReservedHash/ReservedWords.java
+++ Cocoa Coding/src/ReservedHash/ReservedWords.java
@@ -5,24 +5,29 @@ import java.util.HashMap;
 public class ReservedWords {
     public HashMap<String, String> predefineReserves() {
         HashMap<String, String> reservedWords = new HashMap<String, String>();
-        reservedWords.put("INPUT", "[INPUT]");
-        reservedWords.put("MAIN", "[START]");
-        reservedWords.put("OUTPUT", "[OUTPUT]");
-        reservedWords.put("bool", "[bool]");
-        reservedWords.put("case", "[case]");
-        reservedWords.put("def", "[def]");
-        reservedWords.put("else", "[else]");
-        reservedWords.put("elseif", "[elseif]");
-        reservedWords.put("end", "[end]");
-        reservedWords.put("float", "[float]");
-        reservedWords.put("follow", "[follow]");
-        reservedWords.put("for", "[for]");
-        reservedWords.put("if", "[if]");
-        reservedWords.put("int", "[int]");
-        reservedWords.put("stop", "[stop]");
-        reservedWords.put("str", "[str]");
-        reservedWords.put("switch", "[switch]");
-        reservedWords.put("while", "[while]");
+        reservedWords.put("BOOL", "BOOL");
+        reservedWords.put("CASE", "CASE");
+        reservedWords.put("DEF", "DEF");
+        reservedWords.put("ELSE", "ELSE");
+        reservedWords.put("END", "FINISH");
+        reservedWords.put("FLOAT", "FLOAT");
+        reservedWords.put("FLT2STR", "CONVERT");
+        reservedWords.put("FLT2STR", "CONVERT");
+        reservedWords.put("FOLLOW", "FOLLOW");
+        reservedWords.put("FOR", "FOR");
+        reservedWords.put("IF", "IF");
+        reservedWords.put("INPUT", "INPUT");
+        reservedWords.put("INT", "INTEGER");
+        reservedWords.put("INT2FLT", "CONVERT");
+        reservedWords.put("INT2STR", "CONVERT");
+        reservedWords.put("MAIN", "START");
+        reservedWords.put("OUTPUT", "OUTPUT");
+        reservedWords.put("STOP", "STOP");
+        reservedWords.put("STR", "STRING");
+        reservedWords.put("STR2FLT", "CONVERT");
+        reservedWords.put("STR2INT", "CONVERT");
+        reservedWords.put("SWITCH", "SWITCH");
+        reservedWords.put("WHILE", "WHILE");
         return reservedWords;
     }
 }
